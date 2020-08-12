44eef4abc6aaf2eff0bf7d51ee05e62eb9c677f4
Aner Zakovitch <anerisgreat@gmail.com>
2020-08-12 08:25:22 +0300

    Linking init instead of copying, thanks to custom.el file

--- roles/editor/files/init.el
+++ roles/editor/files/init.el
@@ -1,3 +1,4 @@
 (setq vc-follow-symlinks t)
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (require 'org)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
