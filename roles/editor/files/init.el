f368e72e4261af761e63f63246145307fc5be520
Aner Zakovitch <anerisgreat@gmail.com>
2020-08-22 18:07:46 +0300

    Fixed bug where org file wouldn't expand, added config, added ssh to configuratoin.

--- roles/editor/files/init.el
+++ roles/editor/files/init.el
@@ -1,4 +1,9 @@
 (setq vc-follow-symlinks t)
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (require 'org)
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+; Taken out of definition for org-babel-load-file, want to force.
+(setq conf-file (expand-file-name "config.org" user-emacs-directory))
+(let* ((tangled-conf-file (concat (file-name-sans-extension conf-file) ".el")))
+  (org-babel-tangle-file conf-file tangled-conf-file "emacs-lisp")
+  (load-file tangled-conf-file)
+)
