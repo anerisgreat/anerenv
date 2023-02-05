4e1c80abdcadd27bab0361db669b45e3e2fb4678
Aner Zakobar <aner@zakobar.com>
2023-02-05 12:01:54 +0200

    Formatting

--- roles/editor/files/init.el
+++ roles/editor/files/init.el
@@ -4,7 +4,8 @@
 (require 'org)
 ; Taken out of definition for org-babel-load-file, want to force.
 (setq conf-file (expand-file-name "config.org" user-emacs-directory))
-(let* ((tangled-conf-file (concat (file-name-sans-extension conf-file) ".el")))
+(let* ((conf-file (expand-file-name "config.org" user-emacs-directory))
+       (tangled-conf-file (concat (file-name-sans-extension conf-file) ".el")))
   (org-babel-tangle-file conf-file tangled-conf-file "emacs-lisp")
-  (load-file tangled-conf-file)
-)
+  (load-file tangled-conf-file))
+
